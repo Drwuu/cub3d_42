038be3adebc5354f4adc3dbe51c3f00247fdee35
Drwuu <wu.ludwig@gmail.com>
2021-03-01 02:14:00 +0100

    added type in t_list + reorganized the libft.h

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lwourms <lwourms@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: drwuu <drwuu@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 00:51:53 by drwuu             #+#    #+#             */
-/*   Updated: 2021/01/23 14:13:06 by lwourms          ###   ########lyon.fr   */
+/*   Updated: 2021/03/01 00:53:36 by drwuu            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content, t_type _type)
 {
 	t_list	*new_elem;
 
@@ -21,6 +21,7 @@ t_list	*ft_lstnew(void *content)
 	if (!(new_elem = malloc(sizeof(*new_elem))))
 		return (NULL);
 	new_elem->content = content;
+	new_elem->type = _type;
 	new_elem->next = NULL;
 	return (new_elem);
 }
